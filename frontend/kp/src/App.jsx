877a47e3339187a82e847dd46c3ffa9cf69884e7
Rahdansuwa24 <dewaruci393@gmail.com>
2025-09-30 09:40:05 +0700

    make some constraints on app.jsx

--- frontend/kp/src/App.jsx
+++ frontend/kp/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from "react-router-dom";
 import "./index.css";
 import LandingMapta from "./pages/landingpage.jsx";
 import LoginPage from "./pages/login";
@@ -20,13 +20,59 @@ import PenilaianPIC from "./pages/userpic/penilaianpic.jsx";
 import NilaiPeserta from "./pages/userpeserta/nilaipeserta.jsx";
 import JadwalPeserta from "./pages/userpeserta/jadwalpeserta.jsx";
 import SertifikatPeserta from "./pages/userpeserta/sertifikatpeserta.jsx";
+import {jwtDecode} from "jwt-decode";
+import axios from 'axios';
+import { useEffect } from "react";
 
-function Protection({children}){
+function Protection({allowedRoles, children}){
   const token = localStorage.getItem("token")
-  return token ? children : <Navigate to='/login'/>
-}
+  if(!token){
+    return <Navigate to='/login' replace/>
+  }
+  try{
+    const decoded = jwtDecode(token)
+    const userLevel = decoded.user_level
 
+    const currentTimes = Date.now() / 1000
+    if(decoded.exp < currentTimes){
+      localStorage.removeItem("token")
+      return <Navigate to="/login" replace />;
+    }
+    if(allowedRoles && !allowedRoles.includes(userLevel)){
+      const path =  userLevel === 'admin' ? "/admin-dashboard" :
+                    userLevel === 'siswa' ? "/peserta-jadwal" : 
+                    userLevel === 'pic' ? "/pic-jadwal" : "/login";
+      if(path !== "/login") {
+        return <Navigate to={path} replace />;
+      }
+    }
+    return children
+  }catch(error){
+    console.error("token invalid", error)
+    localStorage.removeItem("token");
+    return <Navigate to="/login" replace />;
+  }
+  
+}
 function App() {
+  useEffect(()=>{
+    const constraints = axios.interceptors.response.use(
+      (response)=> response,
+      (error)=> {
+        const originalRequest = error.config
+
+        if(error.response && error.response.status === 401 && originalRequest.url !== "http://localhost:3000/login"){
+          localStorage.removeItem("token");
+          window.location.href = '/login'; 
+          return Promise.reject(error);
+        }
+        return Promise.reject(error);
+      }
+    )
+    return()=>{
+      axios.interceptors.response.eject(constraints);
+    }
+  }, [])
   return (
     <Router>
       <Routes>
@@ -35,20 +81,20 @@ function App() {
         <Route path="/pendaftaran" element={<FormPendaftaran />} />
         <Route path="/pendaftarandinas" element={<FormPendaftaranDinas />} />
         <Route path="/choose-role" element={<ChooseRole />} />
-        <Route path="/admin-dashboard" element={<Protection><Dashboard /></Protection>} />
-        <Route path="/admin-penerimaan" element={<Protection><Diterima /></Protection>} />
-        <Route path="/admin-penolakan" element={<Protection><Ditolak /></Protection>} />
-        <Route path="/admin-selesai" element={<SelesaiMagang />} />
-        <Route path="/admin-jadwal" element={<Protection><Jadwal /></Protection>} />
-        <Route path="/admin-penilaian" element={<Protection><Penilaian /></Protection>} />
-        <Route path="/admin-data-penilaian" element={<DataPenilaian />} />
-        <Route path="/admin-sertifikat" element={<Sertifikat />} />
+        <Route path="/admin-dashboard" element={<Protection allowedRoles={['admin']}><Dashboard /></Protection>} />
+        <Route path="/admin-penerimaan" element={<Protection allowedRoles={['admin']}><Diterima /></Protection>} />
+        <Route path="/admin-penolakan" element={<Protection allowedRoles={['admin']}><Ditolak /></Protection>} />
+        <Route path="/admin-selesai" element={<Protection allowedRoles={['admin']}><SelesaiMagang /></Protection>} />
+        <Route path="/admin-jadwal" element={<Protection allowedRoles={['admin']}><Jadwal /></Protection>} />
+        <Route path="/admin-penilaian" element={<Protection allowedRoles={['admin']}><Penilaian /></Protection>} />
+        <Route path="/admin-data-penilaian" element={<Protection allowedRoles={['admin']}><DataPenilaian /></Protection>} />
+        <Route path="/admin-sertifikat" element={<Protection allowedRoles={['admin']}><Sertifikat /></Protection>} />
         <Route path="/admin-pic" element={<Protection><AkunPIC /></Protection>} />
-        <Route path="/pic-jadwal" element={<Protection><JadwalPIC /></Protection>} />
-        <Route path="/pic-penilaian" element={<Protection><PenilaianPIC /></Protection>} />
-        <Route path="/peserta-nilai" element={<NilaiPeserta />} />
-        <Route path="/peserta-jadwal" element={<JadwalPeserta />} />
-        <Route path="/peserta-sertifikat" element={<SertifikatPeserta />} />
+        <Route path="/pic-jadwal" element={<Protection allowedRoles={['pic']}><JadwalPIC /></Protection>} />
+        <Route path="/pic-penilaian" element={<Protection allowedRoles={['pic']}><PenilaianPIC /></Protection>} />
+        <Route path="/peserta-nilai" element={<Protection allowedRoles={['siswa']}><NilaiPeserta /></Protection>} />
+        <Route path="/peserta-jadwal" element={<Protection allowedRoles={['siswa']}><JadwalPeserta /></Protection>} />
+        <Route path="/peserta-sertifikat" element={<Protection allowedRoles={['siswa']}><JadwalPeserta /><SertifikatPeserta /></Protection>} />
       </Routes>
     </Router>
   );
