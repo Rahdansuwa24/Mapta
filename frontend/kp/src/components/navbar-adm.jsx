6cd44ef7a10f232a8cf7d476621e380aa57f5d43
vernandav mnrg <vernanda.mhmnrg@gmail.com>
2025-10-24 11:38:19 +0700

    ADJUSTMENTS TO THE NOTIFICATION POPUP DISPLAY IN ADMIN

--- frontend/kp/src/components/navbar-adm.jsx
+++ frontend/kp/src/components/navbar-adm.jsx
@@ -89,7 +89,7 @@ export default function NavbarAdm({ onSearch }) {
 
   useEffect(() => {
     if (showNotif) {
-      const timer = setTimeout(() => setShowNotif(false), 5000);
+      const timer = setTimeout(() => setShowNotif(false), 15000);
       return () => clearTimeout(timer);
     }
   }, [showNotif]);
