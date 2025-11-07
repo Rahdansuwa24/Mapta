fc625c4b624bc332458042da91e5efe74a8bed4f
Rahdansuwa24 <dewaruci393@gmail.com>
2025-11-07 14:19:08 +0700

    modifyng cron for automatting change status from final to non-aktif

--- backend/kp/cron/weeklyCheck.js
+++ backend/kp/cron/weeklyCheck.js
@@ -12,6 +12,16 @@ async function scheduler() {
                 console.log(`Peserta ${row.nama} (ID: ${row.id_peserta_magang}) status diubah menjadi selesai.`);
             }
         }
+
+        const [rowsFinal] = await db.query(`select id_peserta_magang, nama, tanggal_mulai_magang, tanggal_selesai_magang, status_penerimaan from peserta_magang where status_penerimaan = 'Final'`)
+        for(const row of rowsFinal){
+            const tanggalSelesai = dayjs(row.tanggal_selesai_magang);
+            const threeWeeksLater = tanggalSelesai.add(3, 'week');
+            if(threeWeeksLater.isBefore(sekarang) || tigaMingguSetelahSelesai.isSame(sekarang, 'day')){
+                await db.query(`UPDATE peserta_magang SET status_penerimaan = 'Non-Aktif' WHERE id_peserta_magang = ?`, [row.id_peserta_magang])
+                console.log(`Peserta ${row.nama} (ID: ${row.id_peserta_magang}) akun peserta sudah di non-aktifkan.`);
+            }
+        }
         console.log("Pengecekan 2 Hari Sekali Selesai.");
     }catch(error){
         console.error("Terjadi kesalahan:", error.message);
